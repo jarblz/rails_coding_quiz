53f5639e79fa43785b341a94c1c698a6ac2dddcf
jared <jaredables@gmail.com>
2018-04-10 12:09:11 -0700

    adding a feature test that covers the majority of available functionality on the site

--- spec/features/tacos_spec.rb
+++ spec/features/tacos_spec.rb
@@ -2,46 +2,19 @@ require "rails_helper"
 
 RSpec.describe "tacos", type: :feature do
 
-  it "allows user to create taco" do
+  it "allows user to create and delete taco" do
     visit root_path
-    expect(true).to eq(true)
-    
+    choose('Tofu')
+    check('Rice')
+    check('Salsa')
+    fill_in "taco_notes", with: "Test Note"
+    click_button('Order Taco')
+    # ensures that our asyncronous call is complete before we continue
+    expect(page).to have_content('Order Taco', wait: true)
+    # we should now have 2 table rows, including the header row
+    expect(page).to have_css('table tr', count: 2)
+    first('.btn-danger').click()
+    expect(page).to have_content('Empty! Order some tacos!', wait: true)
   end
 
-  # it "login required to create post" do
-  #   visit new_post_path
-  #
-  #   expect(current_path).to eq(login_path)
-  #   expect(page).to have_selector(".notification__alert", text: I18n.t("login_required"))
-  # end
-  #
-  # it "creates new post" do
-  #   pending "this needs to be implemented"
-  # end
-  #
-  # it "allows author to update their post" do
-  #   login_as(post.author)
-  #
-  #   visit post_path(post)
-  #   click_link "Edit"
-  #   fill_in "post[title]", with: "New Title"
-  #   click_button "Update Post"
-  #
-  #   expect(current_path).to eq(post_path(post))
-  #   expect(page).to have_selector(".post__title", text: "New Title")
-  # end
-  #
-  # it "doesn't allow update to someone else's post" do
-  #   pending "this needs to be implemented"
-  # end
-  #
-  # it "allows author to remove their post" do
-  #   login_as(post.author)
-  #
-  #   visit post_path(post)
-  #
-  #   expect { click_link "Delete" }.to change(Post, :count).by(-1)
-  #   expect(current_path).to eq(root_path)
-  #   expect(page).to have_selector(".notification__notice", text: I18n.t("posts.destroy.success"))
-  # end
 end
